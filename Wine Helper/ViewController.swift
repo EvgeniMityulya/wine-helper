e25d24b760d514ab697567c6ea72ed601b205208
iseedeadpeople <evgenni2004@gmail.com>
2024-03-09 12:00:54 +0300

    INIT

---
+++ Wine Helper/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Wine Helper
+//
+//  Created by Евгений Митюля on 3/9/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
